3e07d9512c3c6f805c1fbbf10829680341d84aa7
sunrawat <sun.rawat37@gmail.com>
2023-02-06 16:04:41 +0530

    1 file modified

--- src/slider/slider.component.ts
+++ src/slider/slider.component.ts
@@ -10,12 +10,8 @@ import { CommonModule } from '@angular/common';
 export class SliderComponent {
   @ViewChild('map') map!: ElementRef;
   currentIndex = 0;
-  scale = 1;
-  scaleMax = 1;
-  scaleMin = 1;
-  scaleIncreement = 0.1;
   selectedIndex = '';
-  jump = 1;
+  jump = 3; // change jump index accordingly
   data = [
     { title: 'SAU99' },
     { title: 'SAU1096' },
@@ -33,11 +29,6 @@ export class SliderComponent {
     { title: 'SAU888' },
   ];
   selectedList = this.data.slice(0, 3);
-
-  svgClicked(e: any) {
-    console.log(e.target.id);
-    this.regionSelected(e.target.id);
-  }
   previous() {
     this.currentIndex = this.currentIndex - this.jump;
     this.selectedList = this.data.slice(
@@ -52,23 +43,5 @@ export class SliderComponent {
       this.currentIndex + 3
     );
   }
-  selected(title: string) {
-    console.log(title);
 
-    let index: any = null;
-    this.data.find((r, i) => {
-      if (r.title == title) {
-        index = i;
-      }
-    });
-    const index2 = this.selectedList.find((r, i) => r.title == title);
-    if (!index2) {
-      this.currentIndex = index;
-      this.selectedList = this.data.slice(index, this.currentIndex + 3);
-    }
-  }
-  regionSelected(id: string) {
-    this.selectedIndex = id;
-    this.selected(this.selectedIndex);
-  }
 }
